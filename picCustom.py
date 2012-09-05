62bdab0f64bff06e9884655bbc6ec561f5b35037
Lamine Sissoko <lsissoko91@gmail.com>
2012-09-05 06:55:46 -0400

    minor edits

--- picCustom.py
+++ picCustom.py
@@ -1,9 +1,9 @@
 '''
-picCustom is a Python script that allows you to quickly rename a large
-set of images contained in the same directory.
+This Python script that allows you to quickly rename a large set of
+images contained in the same directory.
 
 Example:
-- The images "asdf.jpg", "893t.jpg", "bjdk.jpg", etc... are in your directory.
+- The images "asdf.jpg", "893t.jpg", etc... are in your directory.
 - They are all images taken at the same concert.
 - You can use this script to rename them "my_concert - 001.jpg",
 "my_concert - 002.jpg", "my_concert - 003.jpg", etc...
@@ -16,11 +16,11 @@ import urllib
 
 def getLocalPics(basedir):
 	# Settings
-	# - fixed image name format = (newString + delim + count + typeOut)
+	# - fixed image name = (newString + delim + count + typeOut)
 	newString = "new_image"	# fixed image base string
-	delim = " - "			# delimiter between newString and count
+	delim = ""				# delimiter
 	count = 1				# image number
-	pad = 2					# image number padding, ex: pad = 4 -> img0001, img0002, ...
+	pad = 2					# image number's zero padding
 	typeIn = ".jpg" 		# filetype of images to fix
 	typeOut = ".jpg" 		# filetype of fixed images
 	
@@ -47,4 +47,5 @@ def createFolder(basedir):
 
 
 if __name__ == "__main__":
-	getLocalPics('C:\Users\Lamine\Desktop\pic_customizer')
+	picDir = "C:\pic_customizer"	# directory containing the images to rename
+	getLocalPics(picDir)
